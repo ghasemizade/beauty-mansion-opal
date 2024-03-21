aac1b186a15be02e6da5091b2fc3119776e3fcb4
Hosein Ghasemizade <ghasemizade.hosein@gmail.com>
2024-03-21 15:19:29 +0330

    fix scroll to

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -1,19 +1,21 @@
 import clsx from 'clsx'
-import Image from 'next/image'
 import React from 'react'
 import Nail from "../../public/nail.jpg"
 import Facial from "../../public/facial.jpg"
 import FaceCare from "../../public/face-care.jpg"
-import Hair from "../../public/hair.jpg"
+import Hair from "../../public/hair-care.jpg"
 import Microblading from "../../public/Microblading.jpg"
+import ServiceImg from './ServicImg'
 
 export default function Services() {
+  const serviceTitle = ["Nail", "Facial", "Microblading", "FaceCare", "Hair"]
+
   return (
     <div className={clsx(
         "w-full",
         "my-10",
-        "flex justify-center items-center flex-col"
-    )}>
+        "flex justify-center items-center flex-col",
+    )} id="services">
         <h2 className={clsx(
             "text-4xl font-bold",
             "bg-[#A1A687] text-white px-10 py-5",
@@ -22,37 +24,17 @@ export default function Services() {
         )}>
             Services
         </h2>
-        <ul className={clsx(
-          "flex justify-center items-center",
-          "gap-1 my-10"
+        <div className={clsx(
+          "my-10",
+          "grid grid-cols-1 md:grid-cols-5 gap-20 md:gap-8",
+          "mx-10",
         )}>
-          <li className={clsx(
-            "w-full h-[500px]",
-            
-          )}>
-            <Image src={Nail} alt='' className="rounded-full"/>
-          </li>
-          <li className={clsx(
-            "w-full h-[500px]"
-          )}>
-            <Image src={Facial} alt='' className="rounded-full"/>
-          </li>
-          <li className={clsx(
-            "w-full h-[500px]"
-          )}>
-            <Image src={Microblading} alt='' className="rounded-full"/>
-          </li>
-          <li className={clsx(
-            "w-full h-[500px]"
-          )}>
-            <Image src={FaceCare} alt='' className="rounded-full"/>
-          </li>
-          <li className={clsx(
-            "w-full h-[500px]"
-          )}>
-            <Image src={Hair} alt='' className="rounded-full"/>
-          </li>
-        </ul>
+          <ServiceImg imgSrc={Nail} imgAlt='Nail' imgTitle={serviceTitle[0]}/>
+          <ServiceImg imgSrc={Facial} imgAlt='Facial' imgTitle={serviceTitle[1]}/>
+          <ServiceImg imgSrc={Microblading} imgAlt='Microblading' imgTitle={serviceTitle[2]}/>
+          <ServiceImg imgSrc={FaceCare} imgAlt='FaceCare' imgTitle={serviceTitle[3]}/>
+          <ServiceImg imgSrc={Hair} imgAlt='Hair' imgTitle={serviceTitle[4]}/>
+        </div>
     </div>
   )
 }
