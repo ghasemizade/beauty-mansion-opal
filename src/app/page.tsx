28dba653ea4a1d4e5471846e5e662ecb6f79c9e7
Hosein Ghasemizade <ghasemizade.hosein@gmail.com>
2024-03-21 15:18:45 +0330

    update content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,14 +7,14 @@ import Scroll from "@/components/Scroll";
 export default function Home() {
   return (
     <main className={clsx(
-      "w-full",
+      // "w-full",
       "flex flex-col items-center justify-center",
-      "p-5",
     )}>
       <Header />
       <Banner />
-      <Scroll/>
-      {/* <Services /> */}
+      <Scroll />
+      <Services />
     </main>
+      
   );
 }
