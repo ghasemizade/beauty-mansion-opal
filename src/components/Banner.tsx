9f2590b10afb541fafca4117bb7e8d5237bbaf1f
Hosein Ghasemizade <ghasemizade.hosein@gmail.com>
2024-03-21 15:18:58 +0330

    fix bug banner

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -12,38 +12,44 @@ export default function Banner() {
   
     return (
     <div className={clsx(
-        "bg-[#A1A687] h-[450px] w-full rounded-[50px]",
+        "w-full ",
         "my-10",
         "overflow-hidden"
-    )}>
-        <div className={clsx(
-            "p-5"
         )}>
-            <h1 className={clsx(
-                "text-white text-5xl font-bold",
-                "leading-relaxed",
-                "w-[350px] h-20 text-"
+        <div className={clsx(
+            "rounded-[50px] mx-10",
+            "bg-[#A1A687]",
+            "h-[450px]"
+        )}>
+            <div className={clsx(
+                "p-5"
             )}>
-                <Typewriter 
-                    options={{
-                        strings: [h1],
-                        autoStart: true,
-                        loop: true
-                    }}
-                />
-            </h1>
-        </div>
-        <div className={clsx(
-            "relative"
-        )}>
-            <Image 
-                src={BannerImg} 
-                width={200}
-                alt='banner-img'
-                className={clsx(
-                    "absolute right-0 bottom-[-350px] ",
-                    "md:w-[350px]"
-                )}/>
+                <h1 className={clsx(
+                    "text-white text-3xl md:text-5xl font-bold tracking-wide",
+                    "leading-relaxed",
+                    "w-[250px] md:w-[400px] h-20"
+                )}>
+                    <Typewriter 
+                        options={{
+                            strings: [h1],
+                            autoStart: true,
+                            loop: true
+                        }}
+                    />
+                </h1>
+            </div>
+            <div className={clsx(
+                "relative"
+            )}>
+                <Image 
+                    src={BannerImg} 
+                    width={200}
+                    alt='banner-img'
+                    className={clsx(
+                        "absolute right-0 bottom-[-350px] ",
+                        "md:w-[350px]"
+                    )}/>
+            </div>
         </div>
     </div>
   )
