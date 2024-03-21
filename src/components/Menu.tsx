7a640fc1cc9c066daa6e42b3b083922b1380a59a
Hosein Ghasemizade <ghasemizade.hosein@gmail.com>
2024-03-21 15:19:13 +0330

    fix scroll to

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -5,18 +5,19 @@ import React from 'react'
 
 
 const navData = [
-    {id: 1, name: "Services", url: "/services"},
-    {id: 2, name: "Gallary", url: "/gallary"},
-    {id: 3, name: "About Us", url: "/about"},
-    {id: 4, name: "Contact Us", url: "/contact"},
+    {id: 1, name: "Services", url: "#services"},
+    {id: 2, name: "Gallary", url: "#gallary"},
+    {id: 3, name: "About Us", url: "#about"},
+    {id: 4, name: "Contact Us", url: "#contact"},
 ]
 
 interface MenuProps {
-  showCatMenu: boolean,
-  setShowCatMenu: (show: boolean) => void
+  showCatMenu?: boolean,
+  setShowCatMenu?: (show: boolean) => void
 }
 
 export default function Menu({showCatMenu, setShowCatMenu}: MenuProps) {
+
     return (
       <ul className={clsx(
         "items-center justify-center flex",
@@ -37,8 +38,8 @@ export default function Menu({showCatMenu, setShowCatMenu}: MenuProps) {
                       "flex justify-center items-center",
                         "transition-all duration-200",
                         "border-2 rounded-full hover:border-[#A1A687]",
-                        "px-2 pt-1"
-                      )}>
+                        "px-2 pt-1",
+                      )} scroll={true}>
                           {item.name}
                       </Link>
                   </li>
