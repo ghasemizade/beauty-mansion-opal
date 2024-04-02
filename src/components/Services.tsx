5b4fca8cf2e6c46ca2441b7616277f2832b90bbd
Hosein Ghasemizade <ghasemizade.hosein@gmail.com>
2024-04-02 18:13:59 +0330

    animate to services

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -1,3 +1,4 @@
+'use client'
 import clsx from 'clsx'
 import React from 'react'
 import Nail from "../../public/nail.jpg"
@@ -6,7 +7,7 @@ import FaceCare from "../../public/face-care.jpg"
 import Hair from "../../public/hair-care.jpg"
 import Microblading from "../../public/Microblading.jpg"
 import ServiceImg from './ServicImg'
-import 'animate.css';
+import { Fade, Slide, Zoom } from "react-awesome-reveal";
 
 export default function Services() {
   const serviceTitle = ["Nail", "Facial", "Microblading", "FaceCare", "Hair"]
@@ -17,16 +18,29 @@ export default function Services() {
         "my-10",
         "flex justify-center items-center flex-col",
     )} id="services">
+      <h2 className={clsx(
+        "text-4xl"
+      )}>SERVICES</h2>
         <div className={clsx(
           "my-10",
           "grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 lg:gap-2 gap-20 md:gap-8",
           "mx-10"
         )}>
-          <ServiceImg imgSrc={Nail} imgAlt='Nail' imgTitle={serviceTitle[0]}/>
-          <ServiceImg imgSrc={Facial} imgAlt='Facial' imgTitle={serviceTitle[1]}/>
-          <ServiceImg imgSrc={Microblading} imgAlt='Microblading' imgTitle={serviceTitle[2]}/>
-          <ServiceImg imgSrc={FaceCare} imgAlt='FaceCare' imgTitle={serviceTitle[3]}/>
-          <ServiceImg imgSrc={Hair} imgAlt='Hair' imgTitle={serviceTitle[4]}/>
+          <Fade triggerOnce>
+            <ServiceImg imgSrc={Nail} imgAlt='Nail' imgTitle={serviceTitle[0]}/>
+          </Fade>
+          <Slide cascade>
+            <ServiceImg imgSrc={Facial} imgAlt='Facial' imgTitle={serviceTitle[1]}/>
+          </Slide>
+          <Zoom>
+            <ServiceImg imgSrc={Microblading} imgAlt='Microblading' imgTitle={serviceTitle[2]}/>
+          </Zoom>
+          <Slide>
+            <ServiceImg imgSrc={FaceCare} imgAlt='FaceCare' imgTitle={serviceTitle[3]}/>
+          </Slide>
+          <Zoom>
+            <ServiceImg imgSrc={Hair} imgAlt='Hair' imgTitle={serviceTitle[4]}/>
+          </Zoom>
         </div>
     </div>
   )
